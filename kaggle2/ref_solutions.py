28d81d20be49d9e8eba1757d49495980b0963f78
shota io <shotaio.eng@gmail.com>
2019-10-30 14:04:03 +0900

    delete

--- kaggle2/ref_solutions.py
+++
@@ -1,5 +0,0 @@
-# Home Credit
-https://www.kaggle.com/ogrellier/lighgbm-with-selected-features
-
-
-# Titanic
--- shellscript2/run.sh
+++
@@ -1,6 +0,0 @@
-for exp in 'HEPG2' 'HUVEC' 'RPE' 'U2OS'
-do
-  python /content/recursion-cellular-image-classification/src/train.py --cell-type $exp --gpus 0 --fp16 --with-plates
-done
-
-python /content/recursion-cellular-image-classification/src/predict.py --gpus 0 --fp16 --with-plates
